c12c079d9792aa5b6380a01a913449c645b0408a
Romualdo Viana <romualdo.viana@outlook.com>
2024-08-26 22:01:16 -0300

    Vue-Composition-Api-Course

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import CartItem from './components/CartItem.vue';
+import useCart from './useCart';
+
+const {items,addItem,removeItem} = useCart()
+
+addItem({id:1,name:'Product 1',price:100,quantity:1});
+
+</script>
+
+
+<template>
+ <CartItem v-for="item in items" :key="item.id" :cartItem="item" @remove="removeItem"/>
+</template>
+
+<style scoped></style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {
+  computed,
+  reactive,
+  toRefs,
+ 
+} from "vue";
+
+const props = defineProps({
+  cartItem: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["remove"]);
+
+const item = reactive(props.cartItem);
+
+const increment = () => item.quantity++;
+
+const decrement = () => item.quantity--;
+
+const total = computed(() => item.price * item.quantity);
+
+const { name, price, quantity } = toRefs(item);
+
+const remove = () => emit("remove", item);
+
+</script>
+
+<template>
+  <h1>{{ name }} : {{ price }} : {{ quantity }}</h1>
+
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <br />
+  <button @click="remove">Remove</button>
+  <h3>Total: {{ total }}</h3>
+</template>
+
+<style scoped></style>
